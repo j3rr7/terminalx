19e14c3bd465eb9a1a29f6ddece20bba0dbf69cd
Jeremy <29926331+j3rr7@users.noreply.github.com>
2026-04-28 15:54:44 +0700

    Init Files

---
+++ src/pages/index.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+console.log("Hello World");
+</script>
+
+<template>Hello World</template>
---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import VueRouter from "vue-router/vite";
+import tailwindcss from "@tailwindcss/vite";
+
+const host = process.env.TAURI_DEV_HOST;
+
+// https://vite.dev/config/
+export default defineConfig({
+  clearScreen: false,
+  plugins: [VueRouter({}), vue(), tailwindcss()],
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+  envPrefix: ["VITE_", "TAURI_ENV_*"],
+  build: {
+    target:
+      process.env.TAURI_ENV_PLATFORM == "windows" ? "chrome105" : "safari13",
+    minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
+});
